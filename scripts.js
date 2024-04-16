ba4fafc306bdae8310bda1779007b7cc5a538d84
Gustavo Lima <gustavolimamendes2@gmail.com>
2024-04-15 23:54:41 -0300

    Leitura de arquivos com promises

--- scripts.js
+++ scripts.js
@@ -1,18 +1,27 @@
-const fs = require('fs');
+const fs = require('fs/promises');
 
 console.log('antes de ler');
 // leitura de arquivos síncrono
 // const a = fs.readFileSync('a.txt').toString();
 // console.log(a);
 
-fs.readFile('a.txt', (erro, data) => {
-    if (erro) {
-        console.log(erro);
-    } else {
-        console.log(data.toString());
-    }
-})
+//const a = fs.readFile('a.txt');
 
+// a.then((data) => {
+//     console.log(data.toString());
+// })
+
+// a.catch(() => {
+//     console.log(erro);
+// })
+
+
+(async function(){
+    const a = await fs.readFile('a.txt');
+    
+    console.log(a.toString());
+
+})();
 
 
 console.log('depois de ler');
